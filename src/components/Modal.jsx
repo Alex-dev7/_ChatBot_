41017adf33daa0e573fce5a9f284f01832431046
Alexei Rusu <rusualexei1995@gmail.com>
2024-03-24 14:32:02 -0400

    opacity for modal transition

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,19 @@
-
 function Modal(props) {
-  return (
-    <div className="modal-container" style={{transform: props.showPopup ? "translateX(0%)" : "translateX(160%)"}}>
-        <h3>Welcome back, {props.name} ! </h3>
-    </div>
-  )
+    return (
+        <div
+            className="modal-container"
+            style={{
+                transform: props.showPopup
+                    ? "translateX(0%)"
+                    : "translateX(160%)",
+                opacity: props.showPopup
+                    ? "1"
+                    : "0",
+            }}
+        >
+            <h3>Welcome back, {props.name} ! </h3>
+        </div>
+    );
 }
 
-export default Modal
+export default Modal;
