8cadd850f0fa0d98de69775a789ed14a6b4b1d7e
Alexei Rusu <rusualexei1995@gmail.com>
2024-04-10 21:10:22 -0400

    .

--- src/Navigation/Dropdown.jsx
+++ src/Navigation/Dropdown.jsx
@@ -11,9 +11,27 @@ function Dropdown({ isOpen }) {
         >
             <li><Link to="#">News/todo</Link></li>
             <li><Link to="/aquality">Air Quality</Link></li>
-            <li><Link to="#">Solar Map</Link></li>
+            {/* <li><Link to="#">Solar Map</Link></li> */}
         </ul>
     );
 }
 
 export default Dropdown;
+
+
+// {
+//     "name": "get_air_quality_data",
+//     "description": "Get the current air quality in e.g. Boston",
+//     "parameters": {
+//       "type": "object",
+//       "properties": {
+//         "symbol": {
+//           "type": "string",
+//           "description": "The air quality index"
+//         }
+//       },
+//       "required": [
+//         "symbol"
+//       ]
+//     }
+//   }
